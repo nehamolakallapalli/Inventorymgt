d52be082376ba517ac914310d77d328b9d41d8cb
MolakalapalliNeha <nehamolakallapalli@gmail.com>
2020-08-20 00:24:05 -0700

    sprint2

---
+++ inventorymgt/src/main/java/com/dxctraining/inventorymgt/item/service/IItemService.java
@@ -0,0 +1,10 @@
+package com.dxctraining.inventorymgt.item.service;
+
+import com.dxctraining.inventorymgt.item.entities.Item;
+
+public interface IItemService {
+	Item findById(int id);
+	Item addItem(Item item);
+	void removeItem(int id);
+
+}
--- inventorymgt/src/main/java/com/dxctraining/inventorymgt/ui/InventoryUi.java
+++ inventorymgt/src/main/java/com/dxctraining/inventorymgt/ui/InventoryUi.java
@@ -8,6 +8,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.dxctraining.inventorymgt.item.entities.Item;
+import com.dxctraining.inventorymgt.item.service.IItemService;
 import com.dxctraining.inventorymgt.supplier.entities.Supplier;
 import com.dxctraining.inventorymgt.supplier.exceptions.InvalidSupplierArgumentException;
 import com.dxctraining.inventorymgt.supplier.exceptions.SupplierNullException;
@@ -18,6 +20,9 @@ public class InventoryUi {
 	
 	@Autowired
 	private ISupplierService supplierService;
+	
+	@Autowired
+	private IItemService itemService;
 	
 	@PostConstruct
 	public void runUi() {
@@ -30,10 +35,31 @@ public class InventoryUi {
 			Supplier supplier3 = new Supplier("cccc");
 			supplierService.addSupplier(supplier3);
 			
+			Item item1 = new Item("phone", supplier1);
+			list.add(item1);
+			itemService.addItem(item1);
+			Item item2 = new Item("Computer", supplier2);
+			list.add(item2);
+			itemService.addItem(item2);
+			Item item3 = new Item("laptop", supplier3);
+			list.add(item3);
+			itemService.addItem(item3);
+			
 			System.out.println("*****Fetching supplier by id*****");
 			int id1 = supplier1.getId();
 			Supplier fetched = supplierService.findById(id1);
 			System.out.println("fetched id is "+fetched.getId()+" fetched supplier = "+fetched.getName());
+			
+			System.out.println("*****Deleting an item******");
+			int itemid3 = item3.getId();
+			itemService.removeItem(itemid3);
+			System.out.println("removed item id is "+itemid3);
+			
+			System.out.println("******Fetching item by id******");
+			int itemid1 = item1.getId();
+			Item itemfetched = itemService.findById(itemid1);
+			System.out.println("fetched item id is "+itemfetched.getId()+" fetched item name is "+itemfetched.getName());
+			
 			
 		}catch (InvalidSupplierArgumentException e) {
 			e.printStackTrace();
